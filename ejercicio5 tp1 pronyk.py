f295241f6906cdbe24415f4a3d821274c55c47ec
franpronyk <166619546+franpronyk@users.noreply.github.com>
2024-04-24 11:12:31 -0300

    Create ejercicio5 tp1 pronyk.py

---
+++ ejercicio5 tp1 pronyk.py
@@ -0,0 +1,7 @@
+datos_personales = {'nombre': 'roberto baggio', 'edad': 26, 'ciudad': 'El silencio invadio todas las calles de italia'}
+print("Nombre:", datos_personales['nombre'])
+datos_personales['edad'] = 200
+
+datos_personales['genero'] = 'masculino'
+
+print("Lista", datos_personales)
