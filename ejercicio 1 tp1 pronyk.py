d2b5622426c69086e64c84ccc138897c8947ee59
franpronyk <166619546+franpronyk@users.noreply.github.com>
2024-04-24 09:25:21 -0300

    Create ejercicio 1 tp1 pronyk.py

---
+++ ejercicio 1 tp1 pronyk.py
@@ -0,0 +1,10 @@
+numeroentero = 10
+numeroflotante = 3.14
+cadena =    'Skibidi'
+es_verdadero = True
+
+print("Numero entero:", numeroentero)
+print("Numero flotante:", numeroflotante)
+print("Cadena:", cadena)
+print("¿Es verdadero?", es_verdadero)
+
